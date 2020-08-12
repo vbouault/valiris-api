ea9718e8d923e564b02300414f119c2c9063e1ed
vbouault <vbouault@hotmail.fr>
2020-08-12 12:42:43 +0200

    header

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-// const cors = require('cors');
+const cors = require('cors');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./docs/swagger.yaml');
 const extractClientLanguage = require('./middlewares/extractClientLanguage');
@@ -10,19 +10,19 @@ const extractToken = require('./middlewares/extractToken');
 const app = express();
 const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000);
 
-process.on('unhandledRejection', error => {
-  console.error('unhandledRejection', JSON.stringify(error), error.stack);
-  process.exit(1);
-});
-process.on('uncaughtException', error => {
-  console.log('uncaughtException', JSON.stringify(error), error.stack);
-  process.exit(1);
-});
-process.on('beforeExit', () => {
-  app.close((err) => {
-    if (err) console.error(JSON.stringify(err), err.stack);
-  });
-});
+// process.on('unhandledRejection', error => {
+//   console.error('unhandledRejection', JSON.stringify(error), error.stack);
+//   process.exit(1);
+// });
+// process.on('uncaughtException', error => {
+//   console.log('uncaughtException', JSON.stringify(error), error.stack);
+//   process.exit(1);
+// });
+// process.on('beforeExit', () => {
+//   app.close((err) => {
+//     if (err) console.error(JSON.stringify(err), err.stack);
+//   });
+// });
 
 // middlewares
 app.use((req, res) => {
@@ -32,7 +32,7 @@ app.use((req, res) => {
   }
 })
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 if (process.env.NODE_ENV !== 'production') {
