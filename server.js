600a4c48f7599c4aa6db65c44177664ec8239ad1
vbouault <vbouault@hotmail.fr>
2020-08-30 14:00:17 +0200

    supp cors

--- server.js
+++ server.js
@@ -10,27 +10,13 @@ const extractToken = require('./middlewares/extractToken');
 const app = express();
 const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000);
 
-// process.on('unhandledRejection', error => {
-//   console.error('unhandledRejection', JSON.stringify(error), error.stack);
-//   process.exit(1);
-// });
-// process.on('uncaughtException', error => {
-//   console.log('uncaughtException', JSON.stringify(error), error.stack);
-//   process.exit(1);
-// });
-// process.on('beforeExit', () => {
-//   app.close((err) => {
-//     if (err) console.error(JSON.stringify(err), err.stack);
-//   });
-// });
-
 // middlewares
-app.use((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  if(req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Headers', 'Accept, Content-Type')
-  }
-})
+// app.use((req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   if(req.method === 'OPTIONS') {
+//     res.setHeader('Access-Control-Allow-Headers', 'Accept, Content-Type')
+//   }
+// })
 app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
